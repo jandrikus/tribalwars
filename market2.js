0b710dad65bcde204dde42b5de43fe07e0588448
jandrikus <alejandroalvarezdegracia@hotmail.com>
2016-06-30 00:12:40 +0200

    clean

--- market2.js
+++ market2.js
@@ -1,13 +1,13 @@
 function check(){
 	var hiddenFrameUrl = '/game.php?village='+game_data.village.id+'&screen=market&mode=exchange';
-	var hiddenFrame = createHiddenFrame(hiddenFrameUrl,TWBot.attacks.frameLoaded(hiddenFrame));
+	hiddenFrame = createHiddenFrame(hiddenFrameUrl,TWBot.attacks.frameLoaded);
 };
-function frameLoaded(hiddenFrame){
-	var b=hiddenFrame.contents().find('#bot_check');
-	var c = hiddenFrame.contents().find('img[src="/human.png"]');
-	var wood = hiddenFrame.contents().find('#premium_exchange_rate_wood').find('.premium-exchange-sep').first().text().substr(1);
-	var stone = hiddenFrame.contents().find('#premium_exchange_rate_stone').find('.premium-exchange-sep').first().text().substr(1);
-	var iron = hiddenFrame.contents().find('#premium_exchange_rate_iron').find('.premium-exchange-sep').first().text().substr(1);
+function frameLoaded(){
+	var b=$('#bot_check');
+	var c = $('img[src="/human.png"]');
+	var wood = $('#premium_exchange_rate_wood').find('.premium-exchange-sep').first().text().substr(1);
+	var stone = $('#premium_exchange_rate_stone').find('.premium-exchange-sep').first().text().substr(1);
+	var iron = $('#premium_exchange_rate_iron').find('.premium-exchange-sep').first().text().substr(1);
 	console.log(wood+' '+stone+' '+iron);
 	console.log('here');
 	if(b.size()!==0||c.size()!==0){
@@ -17,6 +17,6 @@ function frameLoaded(hiddenFrame){
 	hiddenFrame.remove();
 };
 function createHiddenFrame(a,b){
-	return $('<iframe src="'+a+'" />').load(b).css({width:'100px',height:'100px',position:'absolute',left:'-1000px'}).appendTo('body');
+	return $('<iframe src="'+a+'" />').on('load', b).css({width:'100px',height:'100px',position:'absolute',left:'-1000px'}).appendTo('body');
 };
 setInterval(check,30000);
