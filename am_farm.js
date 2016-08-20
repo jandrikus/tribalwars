59eb9a5451f7dd0e76b1ffdc822282c0a6e5b9cf
jandrikus <jandrikus@users.noreply.github.com>
2016-08-20 19:03:58 +0200

    Add files via upload

--- am_farm.js
+++ am_farm.js
@@ -1,8 +1,22 @@
 function farm() {
-	lista = [];
-	for (i=1; i<5; i++){
-		window.location.search += '&farm_page='+i;
-		$('#plunder_list').children().find('tr').each(function(b,a){lista.push(a.id)})
+	lista = load('lista');
+	if (lista == null){
+		lista = []
 	}
+	$('#plunder_list').children().find('tr').each(function(b,a){lista.push(a.id)});
+	store('lista', lista);
 	console.log(lista)
+}
+function store(a, b, c) {
+	if (c) {
+		localStorage.setItem(game_data.world + '_' + game_data.village.id + '_' + a, JSON.stringify(b))
+	} else {
+		localStorage.setItem(game_data.world + '_' + game_data.village.id + '_' + a, b)
+	}
+}
+function load(a, b) {
+	if (b) {
+		return JSON.parse(localStorage.getItem(game_data.world + '_' + game_data.village.id + '_' + a))
+	}
+	return localStorage.getItem(game_data.world + '_' + game_data.village.id + '_' + a)
 }
