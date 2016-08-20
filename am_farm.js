9225ddbc4ba7ef4609a1b1f0487fd893a4cc5633
jandrikus <jandrikus@users.noreply.github.com>
2016-08-20 18:47:05 +0200

    Add files via upload

--- am_farm.js
+++ am_farm.js
@@ -1,6 +1,6 @@
 function farm() {
 	lista = [];
-	for (i=1, i<5, i++){
+	for (i=1; i<5; i++){
 		window.location.search += '&farm_page='+i;
 		$('#plunder_list').children().find('tr').each(function(b,a){lista.push(a.id)})
 	}
